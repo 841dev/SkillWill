e6294445051a33f0d12baf1fa01e37acb7e04060
841dev <dbaiashvili@gmail.com>
2023-12-14 00:12:40 +0400

    20. Car and Animal

--- 20. Car n Animal/car_animal/car/admin.py
+++ 20. Car n Animal/car_animal/car/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import car
 
 # Register your models here.
+class CarAdmin(admin.ModelAdmin):
+    list_display = ('id', 'brand', 'model', 'car_type')
+
+admin.site.register(car, CarAdmin)
